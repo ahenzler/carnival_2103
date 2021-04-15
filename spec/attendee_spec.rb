b8166c963237a2b74ad13d100f0f73f0e476fe31
ahenzler <aliciahenzler@outlook.com>
2021-04-15 09:15:31 -0600

    create spec for attendee

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,28 @@
+require './lib/ride'
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  context 'exists' do
+     attendee = Attendee.new('Bob', 20)
+
+    it 'is a attendee' do
+      expect(attendee).to be_instance_of(Attendee)
+    end
+
+    it 'has attributes' do
+
+      expect(attendee.name).to eq('Bob')
+      expect(attendee.spending_money).to eq(20)
+      expect(attendee.interests).to eq([])
+    end
+  end
+
+  context 'can modify attributes' do
+    attendee = Attendee.new('Bob', 20)
+
+    it 'can add interests' do
+      expected = ["Bumper Cars", "Ferris Wheel"]
+      expect(attendee.interests).to eq(expected)
+    end
+  end
+end
