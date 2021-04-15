d9cde9963dddb9d0d870e72dda771e6590e69188
ahenzler <aliciahenzler@outlook.com>
2021-04-15 09:11:04 -0600

    complete ride class

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,12 +4,11 @@ RSpec.describe Ride do
   context 'exists' do
     ride = Ride.new({name: 'Ferris Wheel', cost: 0})
 
-    it 'exists' do
+    it 'is a ride' do
       expect(ride).to be_instance_of(Ride)
     end
 
     it 'has attributes' do
-
       expect(ride.name).to eq('Ferris Wheel')
       expect(ride.cost).to eq(0)
     end
